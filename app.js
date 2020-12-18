db345a6d1eeec7001d95a50f1c51878e7c4a6b26
MohammadHassanRao <hassanribery0335@gmail.com>
2020-12-18 16:07:32 +0500

    hello world express

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require("express");
+
+var server = express();
+
+server.get("/", (req, res, next) => {
+  console.log("someone got menu");
+  res.send("menu:what do you want? food or water?");
+});
+
+server.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
